2570e50b3afa0d989024198124a2ee1af2c20028
Daniel Costa <ccdanielcosta@gmail.com>
2018-05-24 15:05:11 +0100

    Added class SUMOCoordenadas; Added return statements for Vehicle 0; Added a list to return values in Main;

---
+++ src/cam/SUMOCoordenadas.java
@@ -0,0 +1,11 @@
+package cam;
+
+public class SUMOCoordenadas {
+        public final double latitude;
+        public final double longitude;
+
+        public SUMOCoordenadas(double lat, double lon) {
+            latitude = lat;
+            longitude = lon;
+        }
+}
